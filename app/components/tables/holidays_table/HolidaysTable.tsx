2f131efd2c85fb63284f8930250584393fd20048
Prayag Das <prayag.j.das2001@gmail.com>
2023-04-05 18:22:00 -1000

    Created a new component for the table rows and refined the headers component. Implemented new table components into HolidaysTable.tsx

--- app/components/tables/holidays_table/HolidaysTable.tsx
+++ app/components/tables/holidays_table/HolidaysTable.tsx
@@ -1,5 +1,38 @@
-import HolidaysTableData from "./table_element/HolidaysTableData";
-import React, {useMemo} from "react";
-import {useTable} from "react-table";
+import {HolidaysTableHeaders} from "@/components/tables/holidays_table/table_element/HolidaysTableHeaders";
+import HolidaysTable from "@/components/tables/holidays_table/HolidaysTable";
+import HolidaysTableData from "@/components/tables/holidays_table/table_element/HolidaysTableData";
 
+type ColumnDefinitionType<T, K extends keyof T> = {
 
+  /*
+  T is a placeholder for the data object, which will be implemented when the other components are complete
+
+  Trying to Implement the column headers in the HolidaysTable.tsx instead of calling it from a table_element component
+  */
+  key: K;
+  header: string;
+  width?: number;
+}
+
+type TableProps<T, K extends keyof T> = {
+  data: Array<T>;
+  columns: Array<ColumnDefinitionType<T, K>>;
+}
+
+const style = {
+    borderCollapse: 'collapse'
+} as const
+
+const Table = <T, K extends keyof T>({ data, columns }: TableProps<T, K>): JSX.Element => {
+    return (
+        <table style={style}>
+            <HolidaysTableHeaders columns={columns} />
+            <HolidaysTableRows
+                data={data}
+                columns={columns}
+            />
+        </table>
+    );
+};
+
+export default Table;
