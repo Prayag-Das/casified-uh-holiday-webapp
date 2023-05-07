63ec8dc93d7b59a25398f597c1d8b8591431b9a6
Prayag Das <prayag.j.das2001@gmail.com>
2023-05-06 19:03:42 -1000

    Still can't find issues with columns

--- app/components/tables/holidays_table/HolidaysTable.tsx
+++ app/components/tables/holidays_table/HolidaysTable.tsx
@@ -1,84 +1,44 @@
+import React, {useMemo} from "react";
+import { useTable } from "react-table";
 import holidaysData from "../../../public/data/holidaysData.json";
-import * as React from "react";
-import {useTable} from "react-table";
-import {COLUMNS} from "@/components/tables/holidays_table/table_element/HolidaysTableColumns";
-import {useMemo} from "react";
-const HolidaysTable = () => {
+import { COLUMNS } from "@/components/tables/holidays_table/table_element/HolidaysTableColumns";
+import isTableElement from "@popperjs/core/lib/dom-utils/isTableElement";
+import "./table_element/table.css"
+
+export const BasicTable = () => {
 
     const columns = useMemo(() => COLUMNS, [])
-    const data = useMemo(() => holidaysData, [])
-    useTable({
-        columns: COLUMNS,
-        data: holidaysData,
-    })
+    const data = useMemo(() => holidaysData,[])
     const tableInstance = useTable({
         columns,
-        data,
+        data
     })
-    const {
-        getTableProps,
-        getTableBodyProps,
-        headerGroups,
-        rows,
-        prepareRow,
-    } = tableInstance;
 
-  return (
-        <table {...getTableProps()}>
-            <thead>
-                {
-                  {headerGroups.map(headerGroups => (
-                        <tr {...headerGroup.getHeaderGroupProps()}>
-                            {headerGroup.headers.map((column) =>
-                                <th {...column.getHeaderProps()}>{column.render('Header')}</th>
-                            )}
-                        </tr>
-                    ))}
-                }
-            </thead>
-            <tbody {...getTableBodyProps()}>
-                {
-                    rows.map(row => {
-                        prepareRow(row)
-                        return (
-                            <tr {...row.getRowProps()}>
-                                {
-                                    row.cells.map( cell => {
-                                        return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
-                                    })
-                                }
-                            </tr>
-                        )
-                    })
-                }
-            </tbody>
-            <tfoot>
-                <tr>
-                    <th></th>
-                </tr>
-            </tfoot>
-        </table>
-    )
-
-    /* These contents will be added to the HolidaysTableData in the future
-    const [data, setData] = React.useState([])
-
-    React.useEffect(() => {
-
-        HolidaysTableData().then(res => {
-            setData(res.data)
-        })
-    },[])
+    const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = tableInstance;
 
     return (
-        <div>
-            <pre>
-                <code> {JSON.stringify(data, null, 2)} </code>
-            </pre>
-        </div>
+        <table {...getTableProps()}>
+            <thead>
+                {headerGroups.map((headerGroup) => (
+                    <tr {...headerGroup.getHeaderGroupProps()}>
+                        {headerGroup.headers.map((column) => (
+                            <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+                        ))}
+                    </tr>
+                ))}
+            </thead>
+            <tbody {...getTableBodyProps}>
+                {rows.map((row) => {
+                    prepareRow(row)
+                    return(
+                        <tr {...row.getRowProps()}>
+                            {row.cells.map((cell) => {
+                                return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                            })}
+                        </tr> 
+                    )
+                })}
+            </tbody>
+        </table>
     )
-    
-   */
 }
-
-export default HolidaysTable;
