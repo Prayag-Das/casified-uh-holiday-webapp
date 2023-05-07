afcb9b751debc39702b7c74aa8aae58f8dfda725
Prayag Das <prayag.j.das2001@gmail.com>
2023-05-06 17:01:07 -1000

    Completed HolidaysTable.tsx

--- app/components/tables/holidays_table/HolidaysTable.tsx
+++ app/components/tables/holidays_table/HolidaysTable.tsx
@@ -1,9 +1,64 @@
 import holidaysData from "../../../public/data/holidaysData.json";
 import * as React from "react";
 import {useTable} from "react-table";
-
+import {COLUMNS} from "@/components/tables/holidays_table/table_element/HolidaysTableColumns";
+import {useMemo} from "react";
 const HolidaysTable = () => {
 
+    const columns = useMemo(() => COLUMNS, [])
+    const data = useMemo(() => holidaysData, [])
+    useTable({
+        columns: COLUMNS,
+        data: holidaysData,
+    })
+    const tableInstance = useTable({
+        columns,
+        data,
+    })
+    const {
+        getTableProps,
+        getTableBodyProps,
+        headerGroups,
+        rows,
+        prepareRow,
+    } = tableInstance;
+
+  return (
+        <table {...getTableProps()}>
+            <thead>
+                {
+                  {headerGroups.map(headerGroups => (
+                        <tr {...headerGroup.getHeaderGroupProps()}>
+                            {headerGroup.headers.map((column) =>
+                                <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+                            )}
+                        </tr>
+                    ))}
+                }
+            </thead>
+            <tbody {...getTableBodyProps()}>
+                {
+                    rows.map(row => {
+                        prepareRow(row)
+                        return (
+                            <tr {...row.getRowProps()}>
+                                {
+                                    row.cells.map( cell => {
+                                        return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                                    })
+                                }
+                            </tr>
+                        )
+                    })
+                }
+            </tbody>
+            <tfoot>
+                <tr>
+                    <th></th>
+                </tr>
+            </tfoot>
+        </table>
+    )
 
     /* These contents will be added to the HolidaysTableData in the future
     const [data, setData] = React.useState([])
---
+++ app/components/tables/holidays_table/table_element/HolidaysTableColumns.tsx
@@ -0,0 +1,14 @@
+export const COLUMNS = [
+    {
+        Header: 'Holiday',
+        accessor: 'description'
+    },
+    {
+        Header: 'Observed',
+        accessor: 'observedDateFull'
+    },
+    {
+        Header: 'Official',
+        accessor: 'officialDateFull'
+    }
+]
