baa61246606350946ade988511e39bf8c17ebe77
Prayag Das <prayag.j.das2001@gmail.com>
2023-05-31 18:08:14 -1000

    Included pagination, improved filter code by adding new constraints on filtered data, but still needs work.

--- app/components/tables/holidays_table/HolidaysTable.tsx
+++ app/components/tables/holidays_table/HolidaysTable.tsx
@@ -1,6 +1,6 @@
-import { useMemo, useState } from 'react';
-import { Table } from 'react-bootstrap';
-import { useTable, Column, useGlobalFilter } from 'react-table';
+import React, { useState, useEffect } from 'react';
+import { Table, Pagination } from 'react-bootstrap';
+import { useTable, Column } from 'react-table';
 import { Holiday } from '../../../types/Holiday';
 import HolidaysTableHeaders from '@/components/tables/holidays_table/table_element/HolidaysTableHeaders';
 
@@ -8,15 +8,33 @@ interface HolidaysTableProps {
   data: Holiday[];
 }
 
-const HolidaysTable = ({ data }: HolidaysTableProps) => {
-    const columns: Column[] = useMemo(() => HolidaysTableHeaders, []);
-    const [filterValue, setFilterValue] = useState<string>(''); //no initial filter text in field
+const columns: Column[] = HolidaysTableHeaders; // Static columns
 
-    const filteredData = useMemo(() => {
-        return data.filter((holiday) =>
-            holiday.description.toLowerCase().includes(filterValue.toLowerCase())
+const HolidaysTable = ({ data }: HolidaysTableProps) => {
+    const [filterValue, setFilterValue] = useState<string>('');
+    const [currentPage, setCurrentPage] = useState(1);
+    const itemsPerPage = 5;
+    const [tableData, setTableData] = useState<Holiday[]>([]); // Initialize table data as an empty array
+
+    useEffect(() => {
+        const filteredData = data.filter((holiday) =>
+            holiday.description.toLowerCase().startsWith(filterValue.toLowerCase())
         );
-    }, [data, filterValue]);
+        setTableData(filteredData); // Set table data to the filtered data
+        setCurrentPage(1); // Reset current page to 1
+    }, [filterValue, data]);
+
+    const totalPages = Math.ceil(tableData.length / itemsPerPage);
+    const pageRange = 6;
+    let startPage = Math.max(1, currentPage - Math.floor(pageRange / 2));
+    const endPage = Math.min(startPage + pageRange - 1, totalPages);
+    if (endPage === totalPages) {
+        startPage = Math.max(1, endPage - pageRange + 1);
+    }
+
+    const indexOfLastItem = currentPage * itemsPerPage;
+    const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+    const currentItems = tableData.slice(indexOfFirstItem, indexOfLastItem);
 
     const {
         getTableProps,
@@ -24,11 +42,14 @@ const HolidaysTable = ({ data }: HolidaysTableProps) => {
         headerGroups,
         rows,
         prepareRow,
-    } = useTable({ columns, data: filteredData, useGlobalFilter });
+    } = useTable({ columns, data: currentItems });
 
-    //this is called whenever the user changes the text in the input field (line 39)
     const handleFilterChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         setFilterValue(event.target.value);
+    };
+
+    const handlePageChange = (pageNumber: number) => {
+        setCurrentPage(pageNumber);
     };
 
     return (
@@ -66,6 +87,38 @@ const HolidaysTable = ({ data }: HolidaysTableProps) => {
                     })}
                 </tbody>
             </Table>
+            <Pagination>
+                {startPage > 1 && (
+                    <>
+                        <Pagination.Item onClick={() => handlePageChange(1)}>
+              1
+                        </Pagination.Item>
+                        {startPage > 2 && <Pagination.Ellipsis disabled />}
+                    </>
+                )}
+                {Array(endPage - startPage + 1)
+                    .fill(null)
+                    .map((_, index) => {
+                        const pageNumber = startPage + index;
+                        return (
+                            <Pagination.Item
+                                key={pageNumber}
+                                active={pageNumber === currentPage}
+                                onClick={() => handlePageChange(pageNumber)}
+                            >
+                                {pageNumber}
+                            </Pagination.Item>
+                        );
+                    })}
+                {endPage < totalPages && (
+                    <>
+                        {endPage < totalPages - 1 && <Pagination.Ellipsis disabled />}
+                        <Pagination.Item onClick={() => handlePageChange(totalPages)}>
+                            {totalPages}
+                        </Pagination.Item>
+                    </>
+                )}
+            </Pagination>
         </div>
     );
 };
