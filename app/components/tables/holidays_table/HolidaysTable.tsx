880109612e74b8defc5588bbffd3f6a802c0c1f9
Prayag Das <prayag.j.das2001@gmail.com>
2023-06-02 18:25:23 -1000

    Troubleshooting for react-table filtering and pagination

--- app/components/tables/holidays_table/HolidaysTable.tsx
+++ app/components/tables/holidays_table/HolidaysTable.tsx
@@ -3,27 +3,33 @@ import { Table, Pagination } from 'react-bootstrap';
 import { useTable, Column } from 'react-table';
 import { Holiday } from '../../../types/Holiday';
 import HolidaysTableHeaders from '@/components/tables/holidays_table/table_element/HolidaysTableHeaders';
-
 interface HolidaysTableProps {
   data: Holiday[];
 }
 
-const columns: Column[] = HolidaysTableHeaders; // Static columns
+const columns: Column[] = HolidaysTableHeaders;
 
 const HolidaysTable = ({ data }: HolidaysTableProps) => {
+    //14-17: Initialization
     const [filterValue, setFilterValue] = useState<string>('');
     const [currentPage, setCurrentPage] = useState(1);
     const itemsPerPage = 5;
-    const [tableData, setTableData] = useState<Holiday[]>([]); // Initialize table data as an empty array
-
+    const [tableData, setTableData] = useState<Holiday[]>([]);
+  
+    /*
+      useEffect(): used when user input changes to fetch and store filtered data,
+      which is used to render the new table. Sets current page to 1.
+    */
     useEffect(() => {
         const filteredData = data.filter((holiday) =>
             holiday.description.toLowerCase().startsWith(filterValue.toLowerCase())
         );
-        setTableData(filteredData); // Set table data to the filtered data
-        setCurrentPage(1); // Reset current page to 1
+        console.log(filteredData);
+        setTableData(filteredData);
+        setCurrentPage(1);
     }, [filterValue, data]);
 
+    //33-39: Uses tableData.length to calculate amount of pages necessary. Limits Pagination bar's page range to 6.
     const totalPages = Math.ceil(tableData.length / itemsPerPage);
     const pageRange = 6;
     let startPage = Math.max(1, currentPage - Math.floor(pageRange / 2));
@@ -32,10 +38,12 @@ const HolidaysTable = ({ data }: HolidaysTableProps) => {
         startPage = Math.max(1, endPage - pageRange + 1);
     }
 
+    //42-44: Splits table data evenly among totalPages.
     const indexOfLastItem = currentPage * itemsPerPage;
     const indexOfFirstItem = indexOfLastItem - itemsPerPage;
     const currentItems = tableData.slice(indexOfFirstItem, indexOfLastItem);
 
+    //47-53: Initializes Table with the correct properties, columns, and headers by the useTableFunction
     const {
         getTableProps,
         getTableBodyProps,
@@ -44,14 +52,20 @@ const HolidaysTable = ({ data }: HolidaysTableProps) => {
         prepareRow,
     } = useTable({ columns, data: currentItems });
 
+    //handleFilterChange(): changes filter input field to display user input
     const handleFilterChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         setFilterValue(event.target.value);
     };
 
+    //handlePageChange(): changes the current page to the value passed in
     const handlePageChange = (pageNumber: number) => {
         setCurrentPage(pageNumber);
     };
 
+    /*
+      66-135: renders the input field, table, and pagination bar
+      (each with their own uses for the functions defined above)
+    */
     return (
         <div>
             <input
