d307a120e36c4f0c85e1ea1a69bbb960f87f929c
Prayag Das <prayag.j.das2001@gmail.com>
2023-11-30 15:48:32 -1000

    Added more tests for HolidaysTable sorting. Added Pagination test class.
    Started working on Pagination test class.
    
    removed page incrementing test from Pagination test class
    
    Added and implemented Filter test class.
    
    Finished Pagination component's test class
    
    Restructured Pagination buttons to use a linked list for O(1) operations

--- app/components/tables/holidays_table/table_element/Pagination.tsx
+++ app/components/tables/holidays_table/table_element/Pagination.tsx
@@ -1,73 +1,103 @@
-import {useState} from "react";
+import { useState, useRef } from 'react';
+import { useReactTable } from '@tanstack/react-table';
+import { Holiday } from '@/components/tables/holidays_table/Holiday';
 
-interface paginationProps {
-    tableInstance : any
+interface Node {
+  value: number;
+  next?: Node | null;
 }
-const Pagination = ({tableInstance} : paginationProps) => {
-    const [activePage, setActivePage] = useState(0);
-    const startPage = Math.max(0, activePage - 2);
-    const endPage = Math.min(tableInstance.getPageCount() - 1, startPage + 4);
 
-    const paginationButtons = [];
-    for (let i = startPage; i <= endPage; i++) {
-        paginationButtons.push(
-            <button
-                key={i}
-                className={`btn btn-outline-success ${activePage === i ? 'active' : ''}`}
-                onClick={() => {
-                    tableInstance.setPageIndex(i);
-                    setActivePage(i); // Update the active page when a button is clicked
-                }}
-            >
-                {i + 1}
-            </button>
-        );
+interface PaginationProps {
+  tableInstance: ReturnType<typeof useReactTable<Holiday>>;
+}
+
+const Pagination = ({ tableInstance }: PaginationProps) => {
+  const [activePage, setActivePage] = useState(0);
+  const startPage = Math.max(0, activePage - 2);
+  const endPage = Math.min(tableInstance.getPageCount() - 1, startPage + 4);
+
+  const paginationButtonsRef = useRef<Node | null>(null);
+
+  const addPageToLinkedList = (page: number) => {
+    const newNode: Node = { value: page, next: paginationButtonsRef.current };
+    paginationButtonsRef.current = newNode;
+  };
+
+  const removeFirstPageFromLinkedList = () => {
+    if (!paginationButtonsRef.current) {
+      return;
     }
-    return (
-        <div className="d-flex justify-content-end">
-            <div className="btn-group">
-                <button className="btn btn-outline-success" onClick={() => {
-                    tableInstance.setPageIndex(0);
-                    setActivePage(0);
-                }}>
-                        &lt;&lt; First
-                </button>
-                <button
-                    className="btn btn-outline-success"
-                    disabled={!tableInstance.getCanPreviousPage()}
-                    onClick={() =>
-                    {
-                        tableInstance.previousPage();
-                        setActivePage(activePage - 1);
-                    }}
-                >
-                        &lt; Prev
-                </button>
-                {paginationButtons.map((u) => u)}
-                <button
-                    className="btn btn-outline-success"
-                    disabled={!tableInstance.getCanNextPage()}
-                    onClick={() =>
-                    {
-                        tableInstance.nextPage();
-                        setActivePage(activePage + 1);
-                    }}
-                >
-                        Next &gt;
-                </button>
-                <button
-                    className="btn btn-outline-success"
-                    onClick={() => {
-                        tableInstance.setPageIndex(tableInstance.getPageCount() - 1);
-                        setActivePage(tableInstance.getPageCount() - 1);
-                    }}
-                >
-                        Last &gt;&gt;
-                </button>
-            </div>
-        </div>
+    paginationButtonsRef.current = paginationButtonsRef.current.next || null;
+  };
+
+  const paginationButtons = [];
+
+  for (let i = startPage; i <= endPage; i++) {
+    paginationButtons.push(
+      <button
+        key={i}
+        className={`btn btn-outline-success ${activePage === i ? 'active' : ''}`}
+        onClick={() => {
+          tableInstance.setPageIndex(i);
+          setActivePage(i);
+        }}
+      >
+        {i + 1}
+      </button>
     );
-}
+    addPageToLinkedList(i);
+  }
+
+  const navigateToPage = (page: number) => {
+    tableInstance.setPageIndex(page);
+    setActivePage(page);
+  };
+
+  return (
+    <div className="d-flex justify-content-end">
+      <div className="btn-group">
+        <button className="btn btn-outline-success" onClick={() => navigateToPage(0)}>
+          &lt;&lt; First
+        </button>
+        <button
+          className="btn btn-outline-success"
+          disabled={!tableInstance.getCanPreviousPage()}
+          onClick={() => {
+            tableInstance.previousPage();
+            removeFirstPageFromLinkedList();
+            addPageToLinkedList(startPage - 1);
+            setActivePage(activePage - 1);
+          }}
+        >
+          &lt; Prev
+        </button>
+        {paginationButtons.map((u) => u)}
+        <button
+          className="btn btn-outline-success"
+          disabled={!tableInstance.getCanNextPage()}
+          onClick={() => {
+            tableInstance.nextPage();
+            removeFirstPageFromLinkedList();
+            addPageToLinkedList(endPage + 1);
+            setActivePage(activePage + 1);
+          }}
+        >
+          Next &gt;
+        </button>
+        <button
+          className="btn btn-outline-success"
+          onClick={() => {
+            const lastPage = tableInstance.getPageCount() - 1;
+            tableInstance.setPageIndex(lastPage);
+            setActivePage(lastPage);
+          }}
+        >
+          Last &gt;&gt;
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default Pagination;
 
