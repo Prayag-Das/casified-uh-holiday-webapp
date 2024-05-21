827103d48847bd123384d2a9c30689e6c2c46bf2
Prayag Das <prayag.j.das2001@gmail.com>
2024-05-21 13:39:27 -1000

    ColorSchemeToggle.test.tsx can now read the current theme of the page

--- app/__tests__/components/layout/header/color_scheme_toggle/ColorSchemeToggle.test.tsx
+++ app/__tests__/components/layout/header/color_scheme_toggle/ColorSchemeToggle.test.tsx
@@ -1,6 +1,19 @@
+beforeAll(() => {
+    window.matchMedia = jest.fn().mockImplementation(query => ({
+        matches: false,
+        media: query,
+        onchange: null,
+        addListener: jest.fn(), // Deprecated
+        removeListener: jest.fn(), // Deprecated
+        addEventListener: jest.fn(),
+        removeEventListener: jest.fn(),
+        dispatchEvent: jest.fn(),
+    }));
+});
+
 import ColorSchemeToggle from '@/components/layout/header/color_scheme_toggle/ColorSchemeToggle';
-//import { ThemeProvider as NextThemesProvider, useTheme } from "next-themes"
-import { render, screen, /*fireEvent, renderHook*/ } from '@testing-library/react';
+import { ThemeProvider as NextThemesProvider, useTheme } from "next-themes"
+import { render, screen, fireEvent, renderHook } from '@testing-library/react';
 
 describe('ColorSchemeToggle', () => {
 
@@ -12,25 +25,25 @@ describe('ColorSchemeToggle', () => {
         expect(screen.getByTestId('MoonIcon')).toBeInTheDocument();
     });
 
-    /*it('should change the theme onClick', () => {
+    it('should change the theme onClick', () => {
         render(<ColorSchemeToggle />);
 
         const wrapper = ({ children }: { children: React.ReactNode }) =>
             <NextThemesProvider defaultTheme="light">{ children }</NextThemesProvider>
 
         let view = renderHook(useTheme, { wrapper });
-        expect(view.result.current).toBe('light');
+        expect(view.result.current.systemTheme).toBe('light');
 
         fireEvent.click(screen.getByRole('button', {name: 'Toggle theme'}));
-        fireEvent.click(screen.getByRole('option', { name: 'Dark' }));
+        fireEvent.click(screen.getByRole('button', { name: 'Dark' }));
 
         view = renderHook(useTheme, { wrapper });
-        expect(view.result.current).toBe('dark');
+        expect(view.result.current.systemTheme).toBe('dark');
 
         fireEvent.click(screen.getByRole('button', {name: 'Toggle theme'}));
-        fireEvent.click(screen.getByRole('option', { name: 'Light' }));
+        fireEvent.click(screen.getByRole('button', { name: 'Light' }));
 
         view = renderHook(useTheme, { wrapper });
-        expect(view.result.current).toBe('light');
-    });*/
+        expect(view.result.current.systemTheme).toBe('light');
+    });
 });
